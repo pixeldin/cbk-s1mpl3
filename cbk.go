84c9feb776ccae263b93fe085effd3b360cb2e1b
dinghan <dinghan@huya.com>
2022-04-05 22:10:10 +0800

    apply doc and demo code

--- cbk.go
+++ cbk.go
@@ -2,16 +2,12 @@ package cbk_s1mpl3
 
 // Cbk接口
 type CircuitBreaker interface {
-	// 当前key(api接口)是否可访问
 	CanAccess(key string) bool
-	// 标记访问, 并且如果当前key失败率到达阈值, 则开启熔断
+	// mark api failed, and cool down when with high err rate
 	Failed(key string)
-	// 标记访问, 并且如果当前key已熔断, 则恢复
+	// mark api succeed
 	Succeed(key string)
-	// 返回当前key的熔断状态
-	IsBreak(key string) bool
-	// 返回所有key(api接口)当前状态, 用于查询汇报
-	Status() interface{}
+	//Status() interface{}
 }
 
 type Error struct {
